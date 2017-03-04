55cce4c2980b51203911c3f2398eecf7ca902441
vernetto <vernettop@gmail.com>
2017-03-04 21:32:20 +0100

    pippo

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,42 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	private List<Topic> topics =  new ArrayList<Topic>(Arrays.asList(
+			new Topic("a", "aa", "aaa"),
+			new Topic("b", "bb", "bbb")
+			));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+	
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+}
