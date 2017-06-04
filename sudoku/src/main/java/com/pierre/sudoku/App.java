308f4671b4f3760fa35c506a8ed05aa2fabaff96
vernetto <vernettop@gmail.com>
2017-06-04 22:40:18 +0200

    adding sudoku

---
+++ sudoku/src/main/java/com/pierre/sudoku/App.java
@@ -0,0 +1,16 @@
+package com.pierre.sudoku;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Logger logger = Logger.getLogger(App.class.getName());
+        logger.info("hello world!");
+    }
+}
